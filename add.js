05ca6cc0996d31cbd10c96ac25579fe11da4c346
mgstr <1258626+mgstr@users.noreply.github.com>
2025-01-12 20:17:39 +0200

    implement cleanup after adding a new idea

--- add.js
+++ add.js
@@ -1,5 +1,14 @@
 console.log("extension popup loaded")
 
+function store(key, value) {
+    console.log(`storing key=${key} value=${value}`)
+    chrome.storage.local.set({ "textInput": textInput.value }, () => {
+        if (chrome.runtime.lastError) {
+            console.error("data saving error:", chrome.runtime.lastError)
+        }
+    })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("page loaded...")
 
@@ -11,16 +20,29 @@ document.addEventListener("DOMContentLoaded", () => {
         if (result.textInput) {
             console.log("restoring data in the textInput")
             textInput.value = result.textInput
+            setAddButtonState()
         }
     })
 
     textInput.addEventListener("input", () => {
         console.log("input: ", "'" + textInput.value + "'")
-
-        addButton.disabled = textInput.value.trim().length === 0
-
-        chrome.storage.local.set({ "textInput": textInput.value }, () => {
-            console.log("lastError:", chrome.runtime.lastError)
-        })
+        store("textInput", textInput.value)
+        setAddButtonState()
     })
+
+    addButton.addEventListener("click", () => {
+        console.log("add button clicked")
+        // for now just clear the input text
+        clearInputText()
+    })
+
+    function clearInputText() {
+        textInput.value = ""
+        addButton.disabled = true
+        store("textInput", "")
+    }
+
+    function setAddButtonState() {
+        addButton.disabled = textInput.value.trim().length === 0
+    }
 })
