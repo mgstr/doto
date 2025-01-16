50392b774110bd92c0eb5f2aa43f60002fe2b6a1
mgstr <1258626+mgstr@users.noreply.github.com>
2025-01-17 01:14:26 +0200

    fix align

--- pages/debug.js
+++ pages/debug.js
@@ -2,6 +2,6 @@ import { data } from "../data.js"
 
 export const debug = async (tab, content) => {
     const raw = await data.load()
-    const json = JSON.stringify(raw, 2)
-    content.innerHTML = `<pre>${json}</pre>`
+    const json = JSON.stringify(raw, null, 2)
+    content.innerHTML = `<pre style="text-align: left;">${json}</pre>`
 }
