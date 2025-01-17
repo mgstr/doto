a7b6a07ae23b34352d465bc4f5dd865856ee8163
mgstr <1258626+mgstr@users.noreply.github.com>
2025-01-17 22:19:18 +0200

    backup

--- pages/debug.js
+++ pages/debug.js
@@ -5,9 +5,17 @@ export const debug = async (tab, content) => {
     const json = JSON.stringify(raw, null, 2)
     content.innerHTML = `<pre style="text-align: left;">${json}</pre>`
 
-    tab.querySelector("#buttons").innerHTML = `<button aria-label="close" class="btn">❌</button>`
-    tab.querySelector(`[aria-label="close"]`).addEventListener("click", async (e) => {
+    tab.querySelector("#buttons").innerHTML = `
+        <button aria-label="download" class="btn"><i class="fa-solid fa-download"></i></button>
+        <button aria-label="clear" class="btn"><i class="fa-solid fa-trash"></i></button>
+        `
+
+    tab.querySelector(`[aria-label="clear"]`).addEventListener("click", async (e) => {
         await chrome.storage.local.clear()
         tab.click()
+    })
+
+    tab.querySelector(`[aria-label="download"]`).addEventListener("click", async (e) => {
+        console.log("download")
     })
 }
